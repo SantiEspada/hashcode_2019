3a1e24dd7ca8b9c654a2ee94f0758f9c9ac2f93c
Santi Espada <santi.espada@gmail.com>
2019-02-28 19:36:25 +0100

    update(parser): store output in JSON file

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const parseFile = require('./parser')
+const fs = require('fs')
 
 const INPUT_FILE = './dataset/a_example.txt'
+const OUTPUT_FILE = './out.json'
 
 const main = async () => {
     try {
         const data = await parseFile(INPUT_FILE)
 
-        console.log(data)
+        fs.writeFile(OUTPUT_FILE, JSON.stringify(data), err => {
+            if(err) {
+                console.error(`Error writing output: ${err}`)
+            } else {
+                console.log('Success!');
+            }
+        })
     } catch(error) {
         console.error(error)
     }
--- parser.js
+++ parser.js
@@ -17,17 +17,40 @@ const parseData = (data, resolve, reject) => {
     const numberOfPictures = Number(lines.shift())
     
     if(numberOfPictures === lines.length) {
+        let tags = {}
         const pictures = lines.map((line, id) => {
-            const [ orientation, numberOfTags, ...tags] = line.split(' ')
+            const [ orientation, numberOfTags, ...pictureTags] = line.split(' ')
+
+            if(Number(numberOfTags) !== pictureTags.length) {
+                reject(`[parsing error] Declared number of tags does not match actual number of tags in picture #${id}`)
+            }
+
+            pictureTags.forEach(tag => {
+                if(Array.isArray(tags[tag])) {
+                    tags[tag].push(id)
+                } else {
+                    tags[tag] = [id]
+                }
+            })
 
             return {
                 id,
                 orientation,
-                tags
+                tags: pictureTags
             }
         });
+        
+        const sortedTags =
+            Object.keys(tags)
+                .map(tag => ({ tag, pictures: tags[tag]}))
+                .sort(({ pictures: picturesA }, { pictures: picturesB }) => (
+                    picturesA.length > picturesB.length ? -1 : 1
+                ))
 
-        resolve(pictures);
+        resolve({
+            tags: sortedTags,
+            pictures
+        });
     } else {
         console.log(lines, lines.length, rows)
         reject('[parsing error] Declared number of pictures does not match number of lines')
