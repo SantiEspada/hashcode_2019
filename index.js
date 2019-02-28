d5659eb8f9707614d50bc08f078204c0b065834a
Santi Espada <santi.espada@gmail.com>
2019-02-28 19:53:33 +0100

    update(options): added flags to set i/o file

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-const parseFile = require('./parser')
+const program = require('commander')
 const fs = require('fs')
 
-const INPUT_FILE = './dataset/a_example.txt'
-const OUTPUT_FILE = './out.json'
+const parseFile = require('./parser')
 
 const main = async () => {
-    try {
-        const data = await parseFile(INPUT_FILE)
+    program
+        .option('-i --input <file>', 'Input file')
+        .option('-o --output <file>', 'Output file (optional, defaults to stdout)')
+        .parse(process.argv)
 
-        fs.writeFile(OUTPUT_FILE, JSON.stringify(data), err => {
-            if(err) {
-                console.error(`Error writing output: ${err}`)
-            } else {
-                console.log('Success!');
-            }
-        })
+    if(!program.input) throw new Error('Input file is required')
+
+    try {
+        const inputFile = program.input
+
+        const data = await parseFile(inputFile)
+        const outputData = JSON.stringify(data)
+
+        if(program.output) {
+            const outputFile = program.output
+
+            fs.writeFile(outputFile, outputData, err => {
+                if(err) {
+                    console.error(`Error writing output: ${err}`)
+                } else {
+                    console.log('Success!');
+                }
+            })
+        } else {
+            process.stdout.write(outputData)
+        }
     } catch(error) {
         console.error(error)
     }
-};
+}
 
 main()
